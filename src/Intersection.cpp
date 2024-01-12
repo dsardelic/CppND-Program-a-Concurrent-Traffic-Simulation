b7b0e7ebea9137f459415a7d0b02c78ff010c0f8
Dino Sardelić <25157560+dsardelic@users.noreply.github.com>
2024-01-12 01:16:49 +0100

    Implement FP.6b

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -93,6 +93,9 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle) {
             << " is granted entry." << std::endl;
 
   // FP.6b : use the methods TrafficLight::getCurrentPhase and TrafficLight::waitForGreen to block the execution until the traffic light turns green.
+  if (_trafficLight.getCurrentPhase() == TrafficLightPhase::red) {
+    _trafficLight.waitForGreen();
+  }
 
   lck.unlock();
 }
